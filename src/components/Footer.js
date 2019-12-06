53e95bf73acb9f03ebf6355cd15d6ff65943e3a9
Alexey_Lisenko <detoner777@gmail.com>
2019-12-06 18:38:57 +0200

    Title.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="social-media">ffaff ffafs</div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Title.js
@@ -0,0 +1,112 @@
+import React, { Component } from "react";
+import meteor from "../img/meteor.gif";
+
+export class Title extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      left: -170,
+      top: 85,
+      width: 0,
+      height: 0,
+      display: null,
+      pagination: null,
+      opacity: 3
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("scroll", this.handleScroll);
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+  handleScroll(e) {
+    let scroll = parseInt(e.target.scrollingElement.scrollTop);
+    let speedFactor = 1;
+    let heightAdjust = 0;
+    let imageWidth = 180;
+    let startPos = 85;
+    let newTop;
+    let newLeft;
+    let displayNoneVal = this.state.width;
+
+    if (this.state.width < 700) {
+      speedFactor = 0.4;
+      heightAdjust = 130;
+      imageWidth = 84;
+      startPos = -200;
+      newLeft = scroll * 3.52 * speedFactor - 170;
+      newTop = scroll * 1.5 * (speedFactor * 2.5) + startPos + heightAdjust;
+      displayNoneVal -= 20;
+    } else {
+      newLeft = scroll * 4 - 170;
+      newTop = scroll * 1.5 + startPos;
+    }
+    let newOpacity;
+    //console.log("left"+this.state.left);
+    //console.log(this.state.width);
+
+    if (newLeft <= this.state.width * 0.1) {
+      this.state.width < 700 ? (newOpacity = 5) : (newOpacity = 3);
+    } else if (
+      newLeft >= this.state.width * 0.1 &&
+      newLeft <= this.state.width * 0.6
+    ) {
+      newOpacity = 8;
+    } else if (newLeft >= this.state.width * 0.6) {
+      this.state.width < 700 ? (newOpacity = 5) : (newOpacity = 3);
+    }
+
+    if (newLeft + imageWidth >= displayNoneVal) {
+      this.setState({
+        display: "none"
+      });
+    } else {
+      this.setState({
+        display: null
+      });
+    }
+
+    this.setState({
+      left: newLeft,
+      top: newTop,
+      opacity: newOpacity
+    });
+  }
+  render() {
+    return (
+      <div className="heading" onScroll={e => this.handleScroll(e)}>
+        <img
+          className="meteor"
+          src={this.state.width > 1000 ? meteor : meteor}
+          style={{
+            position: "absolute",
+            zIndex: 0,
+            left: this.state.left,
+            top: this.state.top,
+            opacity: this.state.opacity / 10,
+            display: this.state.display
+          }}
+        />
+        <h1>Meteorite Explorer App</h1>
+        <div className="key-solution">
+          <p>
+            Explore meteorite strikes across the globe by searching the
+            Meteorite Landing dataset on the Nasa Open Data Portal
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
