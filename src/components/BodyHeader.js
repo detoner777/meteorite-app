ddfc16647b44e2ab84f7237453ee3cb5b23b9bbf
Alexey_Lisenko <detoner777@gmail.com>
2019-12-16 11:11:09 +0200

    css fix

--- src/components/BodyHeader.js
+++ src/components/BodyHeader.js
@@ -5,7 +5,7 @@ export class BodyHeader extends Component {
     return (
       <div className="body-header">
         <div className="body-header-text">
-          <h2>Try it now!</h2>
+          <h2>Data of meteorite falling using NASA data set</h2>
         </div>
       </div>
     );
